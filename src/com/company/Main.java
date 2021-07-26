04556a29b9fd2daca7f01a0d0360040d92c8ff8e
Giftson David <giftson.david@ibo.org>
2021-07-26 22:26:33 +0200

    Testing commit thru intellij

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,12 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        var result2 = 10+20;
+        System.out.println(result2);
+
     }
 }
