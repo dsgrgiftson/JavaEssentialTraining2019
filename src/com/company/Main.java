79972e2bd0d61223c31f8ff183fe9a84c3fc2344
Giftson David <giftsondavid7@gmail.com>
2021-07-27 22:28:41 +0200

    String manipulation charAt, indexOf, substring, subSequence, trim

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var s1 = "Welcome to California!";
+        var s1 = "                   Welcome to California!                         " +
+                "   " +
+                "            ";
+
+        System.out.println(s1.length());
+        System.out.println(s1.indexOf("California"));
+        System.out.println(s1.substring(11));
+        System.out.println(s1.substring(11,15));
+        System.out.println(s1.subSequence(11,15));
+
+        //Removes white spaces in the beginning or the end of the string. Remove spaces, new lines, tabs
+        System.out.println(s1.trim());
     }
 }
